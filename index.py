0eff9c7e7d900189d04d1f4e582ddcc17ccb6fa5
Rogers <rogers@Rogerss-MBP.attlocal.net>
2021-07-03 15:23:55 -0700

    Added another route named test_endpoint to see if naming the endpoint would, and it did.

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,8 +7,15 @@ def index():
 
 app.add_url_rule('/', endpoint='index_func', view_func=index)
 
+
+# Added another route to see if the endpoint name will by routing to the index view function from the test_endpoint. It worked.
+def test_endpoint():
+    return redirect(url_for('index_func'))
+
+app.add_url_rule('/home', endpoint='test_func', view_func=test_endpoint)
+
 """
-    The ur_map is a pointer of the struct Map. I used the pointer to see the route 
+    The url_map is a pointer of the struct Map. I used the pointer to see the route 
     that I created manually with given endpoint name 'index_func'
 """ 
 print(app.url_map)
