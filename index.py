f8637384d079db298ceec9476f6ca84db4f1484d
Rogers <rogers@Rogerss-MBP.attlocal.net>
2021-07-03 12:42:43 -0700

    Added a route manually and an endpoint name.

--- index.py
+++ index.py
@@ -5,4 +5,10 @@ app = Flask(__name__)
 def index():
     return "This is my first app"
 
-app.add_url_rule('/', endpoint='index_fuc', view_func=index)
+app.add_url_rule('/', endpoint='index_func', view_func=index)
+
+"""
+    The ur_map is a pointer of the struct Map. I used the pointer to see the route 
+    that I created manually with given endpoint name 'index_func'
+""" 
+print(app.url_map)
