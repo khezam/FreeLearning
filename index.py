60525872e6b7fd6aa0792b889fe11b6f19f09b00
Rogers <rogers@Rogerss-MBP.attlocal.net>
2021-07-03 16:58:31 -0700

    Added two view fucntion to see the difference between an arguemnt thats in the URL and a query string appended int the URL

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
+# Added an HTML element to this route.
 def index():
-    return "This is my first app"
+    return "<h1>Hello World!</h1>"
 
 app.add_url_rule('/', endpoint='index_func', view_func=index)
+
+# Added a route that fetches the user input appended in the url and display a message with the input of the user.
+# The client has to pass a value in the <name> argement and the argumement is passed to view function.
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Hello {name}</h1>"
+
+# The below view function is different from the above one. The view function checks if the cleint passes a 
+# query string appended in the url
+@app.route('/user')
+def user_query():
+    if not request.args.get('name'):
+        return "<h1>Hello Stranger!</h1>"
+    return f"<h1>Hello {request.args.get('name')}</h1>"
 
 
 # Added another route to see if the endpoint name will by routing to the index view function from the test_endpoint. It worked.
